54766194e978ce7ce1d066e07beb15dde0cd4c82
darija <vivkinaaa@gmail.com>
2025-10-06 19:29:42 +0300

    10.06 Adds
    Added score, background music, plane's velocity and box colliders

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
     public float jumpForce = 10;
+    public int score = 0;
     
     private Rigidbody2D rb;
     
@@ -16,9 +19,27 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) && rb.velocity.y<0)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, -30);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 30);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Point"))
+        {
+            score++;
+            scoreText.text = score.ToString();
         }
     }
 }
